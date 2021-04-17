baa347241c508f4cb29f21cbffb4de84d7931792
Bill Allen <bill@k2bea.org>
2021-04-17 17:37:02 -0400

    get bus values loading and formatted correctly

--- src/Main.elm
+++ src/Main.elm
@@ -5,7 +5,9 @@ import Html exposing (Html, Attribute, div, input, text, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 import Http
-import Json.Decode exposing (Decoder, field, string, map, map2, int, list)
+import Json.Decode exposing (Decoder, field, string, map, map2, int, float, list)
+import Time
+import Debug
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Row as Row
@@ -14,6 +16,9 @@ import Bootstrap.Form.Input as Input
 import Bootstrap.Form.InputGroup as InputGroup
 import Bootstrap.Button as Button
 import Bootstrap.Table as Table
+import Bootstrap.Text exposing ( alignSmRight )
+import FormatNumber exposing (format)
+import FormatNumber.Locales exposing (Locale, usLocale)
 
 
 main = Browser.element {init = init, update = update, view = view, subscriptions = subscriptions}
@@ -21,6 +26,7 @@ main = Browser.element {init = init, update = update, view = view, subscriptions
 type alias Model = { content: String
                    , currently_playing: String
                    , connections: List Connection
+                   , bus_vals: List BusVal
                    }
 
 type alias Parameter = { name: String
@@ -53,10 +59,17 @@ type alias ReturnVal = { currently_playing: String
 
 type alias StopReturn = { currently_playing: String }
 
+type alias BusVals = { bus_vals: List BusVal }
+
+type alias BusVal = { bus_id: Int
+                    , bus_val: Float
+                    }
+
 init : () -> (Model, Cmd Msg)
 init _ = ({ content = ""
           , currently_playing = "stopped"
           , connections = []
+          , bus_vals = []
           }, Cmd.none)
 
 type Msg
@@ -65,6 +78,8 @@ type Msg
     | StopPlaying
     | GotPlay (Result Http.Error ReturnVal)
     | GotStop (Result Http.Error StopReturn)
+    | GotBusVals (Result Http.Error BusVals)
+    | Tick Time.Posix
 
 
 
@@ -107,10 +122,34 @@ update msg model =
                 Err _ ->
                     (model, Cmd.none)
 
+        Tick _ ->
+            let
+                bus_ids = String.join "," (List.map busId
+                                               (List.filter isControl model.connections))
+            in
+                ( model
+                , if model.currently_playing /= "stopped" then
+                      Http.get
+                      { url = "http://localhost:4000/bus_vals/" ++ bus_ids
+                      , expect = Http.expectJson GotBusVals busValsDecoder
+                      }
+                  else Cmd.none
+                )
+
+        GotBusVals result ->
+            case result of
+                Ok rval ->
+                    ({ model | bus_vals = Debug.log "bus_vals" rval.bus_vals
+                     }, Cmd.none
+                    )
+                Err _ ->
+                    (model, Cmd.none)
+
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  Time.every 1000 Tick
 
 view : Model -> Html Msg
 view model =
@@ -127,10 +166,11 @@ view model =
                     , Table.th [] [ text "Description" ]
                     , Table.th [] [ text "From" ]
                     , Table.th [] [ text "To" ]
+                    , Table.th [] [ text "Bus Val" ]
                     ]
             , tbody =
                 Table.tbody []
-                    (List.map connectionRow
+                    (List.map (connectionRow model.bus_vals)
                          (List.filter isControl model.connections))
             }
         tableGrid = if model.currently_playing == "stopped"
@@ -143,7 +183,7 @@ view model =
     div []
         [ CDN.stylesheet
         , Grid.row [ Row.leftXs ]
-            [ Grid.col [Col.xs5]
+            [ Grid.col [Col.xs3]
                    [text ("Currently playing " ++ model.currently_playing) ]
             , Grid.col [ Col.xs4 ] [ InputGroup.config
                                          (InputGroup.text [ Input.placeholder "Example to play"
@@ -159,16 +199,32 @@ view model =
         , tableGrid
         ]
 
+
+busId : Connection -> String
+busId connection = String.fromInt connection.bus_id
+
 isControl : Connection -> Bool
 isControl connection = connection.bus_type == "control"
 
-connectionRow : Connection -> Table.Row msg
-connectionRow connection =
+isBusId : Int -> BusVal -> Bool
+isBusId bus_id bus_val =
+    Debug.log "bus_id" bus_id == Debug.log "bus_val.bus_id" bus_val.bus_id
+
+findFirst : (a -> Bool) -> List a -> Maybe a
+findFirst f vals =
+    List.filter f vals |> List.head
+
+connectionRow : List BusVal -> Connection -> Table.Row msg
+connectionRow bus_vals connection =
     Table.tr []
         [ Table.td [] [ text (String.fromInt connection.bus_id) ]
         , Table.td [] [ text connection.desc ]
         , Table.td [] [ text connection.from_node_param.param_name ]
         , Table.td [] [ text connection.to_node_param.param_name ]
+        , Table.td [ Table.cellAttr <| align "right " ] [ text (case (findFirst (isBusId connection.bus_id) bus_vals) of
+                                Nothing -> ""
+                                Just a -> format { usLocale | decimals = 2 } a.bus_val
+                                )]
         ]
 
 stopDecoder : Decoder StopReturn
@@ -197,3 +253,14 @@ nodeParamDecoder =
     Json.Decode.map2 NodeParam
         ( field "node_id" int )
         ( field "param_name" string)
+
+busValsDecoder : Decoder BusVals
+busValsDecoder =
+    Json.Decode.map BusVals
+        ( field "bus_map" (Json.Decode.list busValDecoder))
+
+busValDecoder : Decoder BusVal
+busValDecoder =
+    Json.Decode.map2 BusVal
+        ( field "bus_id" int )
+        ( field "bus_val" float )
